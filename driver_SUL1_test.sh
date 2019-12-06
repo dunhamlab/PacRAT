67e8f14ec7a961e082467fc9189ea8b8af19a520
Chiann-Ling Yeh <ccyeh@grid-head2.grid.gs.washington.edu>
2019-12-06 13:48:25 -0800

    changed dumb_consensus to gap_consensus; returns more appropriately-lengthed seqs

--- driver_SUL1_test.sh
+++ driver_SUL1_test.sh
@@ -14,11 +14,11 @@ module load python/3.5.2
 module load muscle/3.8.31
 
 # for Clara :) 
-python msa_pacbio.py -d /net/dunham/vol2/Clara/projects/Subassembly_PB_IndelCorrection/test \
-    -o SUL1_test100_seq_barcodes_aligned.txt \
-    --highQual SUL1_test100_combined_minQ0_assignment.tsv \
-    --inputSeqs SUL1_test100_seq_barcodes_filtered.txt \
-    -c 1 -t 0.7
+# python msa_pacbio.py -d /net/dunham/vol2/Clara/projects/Subassembly_PB_IndelCorrection/test \
+#     -o SUL1_test100_seq_barcodes_aligned.txt \
+#     --highQual SUL1_test100_combined_minQ0_assignment.tsv \
+#     --inputSeqs SUL1_test100_seq_barcodes_filtered.txt \
+#     -c 1 -t 0.7
 
 # for Cindy :)
-#python msa_pacbio.py -d /net/dunham/vol2/Cindy/pacbio_git/Subassembly_PB_IndelCorrection/test -o SUL1_test100_seq_barcodes_aligned.txt --highQual SUL1_test100_combined_minQ0_assignment.tsv --inputSeqs SUL1_test100_seq_barcodes_filtered.txt -v
+python msa_pacbio.py -d /net/dunham/vol2/Cindy/pacbio_git/Subassembly_PB_IndelCorrection/test -o SUL1_test100_seq_barcodes_aligned.txt --highQual SUL1_test100_combined_minQ0_assignment.tsv --inputSeqs SUL1_test100_seq_barcodes_filtered.txt -c 2 -t 0.7
