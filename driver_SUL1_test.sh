56626e943ff102b1fd3b61fccd5b54f72b5f2037
cindyyeh <cyeh13@gmail.com>
2020-10-07 10:32:34 -0700

    added conda install/activate

--- driver_SUL1_test.sh
+++ driver_SUL1_test.sh
@@ -4,35 +4,42 @@
 #$ -cwd
 #$ -N msa_pacbio
 
-# for Clara! CHANGEEEE
+# example error/output file locations: for Clara! CHANGEEEE
 # -o /net/dunham/vol2/Clara/projects/Subassembly_PB_IndelCorrection/test
 # -e /net/dunham/vol2/Clara/projects/Subassembly_PB_IndelCorrection/test
 
-# module load python/3.5.2
-# module load muscle/3.8.31
-# module load EMBOSS/6.4.0
 
-# for Clara :) 
-# python msa_pacbio.py -d /net/dunham/vol2/Clara/projects/Subassembly_PB_IndelCorrection/test \
-#     -o SUL1_test100_seq_barcodes_aligned.txt \
-#     --highQual SUL1_test100_combined_minQ0_assignment.tsv \
-#     --inputSeqs SUL1_test100_seq_barcodes_filtered.txt \
-#     -c 1 -t 0.7
+SOURCECONDA=$(conda info --base)
+if [ -z "$SOURCECONDA" ]
+then
+	echo "Conda not available. Please install Anaconda or Miniconda https://docs.conda.io/en/latest/miniconda.html"
+	exit 1
+fi
 
-# for Cindy :)
-#python msa_pacbio.py -d /net/dunham/vol2/Cindy/pacbio_git/200518_subassembly_pairwise/Subassembly_PB_IndelCorrection/test -o SUL1_test100_seq_barcodes_aligned.txt --highQual SUL1_test100_combined_minQ0_assignment.tsv --inputSeqs SUL1_test100_seq_barcodes_filtered.txt -c 2 -t 0.6
-#/net/gs/vol3/software/modules-sw/EMBOSS/6.6.0/Linux/CentOS7/x86_64/EMBOSS-EMBOSS-6.6.0/emboss
+ENVCHECK=$(conda env list | grep "msa_ccs")
+source $SOURCECONDA/etc/profile.d/conda.sh
+if [ -z "$ENVCHECK" ]
+then
+	echo "msa_ccs environment not installed! installing now..."
+	conda env create --file msaccs_env.yml
+	echo "msa_ccs environment installed"
+else
+	echo "msa_ccs environment is installed!"
+fi
 
+conda activate msa_ccs
+echo "Environment activated"
+
+# for running in cluster
 python msa_pacbio.py -d ./test \
 	-o SUL1_test100_seq_barcodes_aligned.txt \
 	--highQual SUL1_test100_combined_minQ0_assignment.tsv \
 	--inputSeqs SUL1_test100_seq_barcodes_filtered.txt -c 2 -t 0.6 \
 	-m /net/gs/vol3/software/modules-sw/muscle/3.8.31/Linux/CentOS7/x86_64/bin/muscle \
 	-n /net/gs/vol3/software/modules-sw/EMBOSS/6.6.0/Linux/CentOS7/x86_64/bin/needle
-	#-m /net/gs/vol3/software/modules-sw/muscle/3.8.31/Linux/RHEL6/x86_64/bin/muscle \
-	#-n /net/gs/vol3/software/modules-sw/EMBOSS/6.6.0/Linux/CentOS7/x86_64/EMBOSS-EMBOSS-6.6.0/emboss/needle
 
-# Cindy Desktop
+
+# example for running on desktop
 # python msa_pacbio.py -d ./test \
 # 	-o SUL1_test100_seq_barcodes_aligned.txt \
 # 	--highQual SUL1_test100_combined_minQ0_assignment.tsv \
