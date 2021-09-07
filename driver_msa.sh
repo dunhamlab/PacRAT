817729fbaba64109c289fa97b1f46fdd967bee75
cindyyeh <cyeh13@gmail.com>
2021-09-07 15:15:41 -0700

    Clarified sections of the script to more clearly distinguish between running locally and on GS cluster. Changed input/output files for command used to run locally.

--- driver_msa.sh
+++ driver_msa.sh
@@ -4,13 +4,14 @@
 #$ -cwd
 #$ -N PacRAT
 
-# For CentOS7 on the cluster
+## There are two sections in this script; please be sure to comment/uncomment as appropriate for your situation
+## ******* Section 1: For CentOS7 on the Genome Sciences cluster ******* #
+## Be sure the comment this section out if you are running locally or on a different cluster environment
 module load python/3.7.7
 module load numpy/1.19.2
 module load biopython/1.77
 module load joblib/0.15.1
 
-# for running in GS cluster
 python msa_pacbio.py -d . -o output/H2B_barcode_variant_map_msa.txt \
 	--highQual input/H2B_combined_minQ0_assignment.tsv \
 	--inputSeqs input/H2B_seq_barcodes.txt \
@@ -18,7 +19,12 @@ python msa_pacbio.py -d . -o output/H2B_barcode_variant_map_msa.txt \
 	-m /net/gs/vol3/software/modules-sw/muscle/3.8.31/Linux/CentOS7/x86_64/bin/muscle \
 	-n /net/gs/vol3/software/modules-sw/EMBOSS/6.6.0/Linux/CentOS7/x86_64/bin/needle
 
-# Running locally with conda environment
+## ******* Section 2: Running locally with conda environment ******* #
+## This is a sample for running PacRAT locally. We have here a skeleton of necessary steps you need to take in order to run PacRAT. Customize as needed.
+## Please look at README file for directions on installing MUSCLE and NEEDLE. You must specify the location of the software.
+## Uncomment this section if you're choosing to run locally
+
+## Validate that Anaconda/Miniconda are installed
 # SOURCECONDA=$(conda info --base)
 # if [ -z "$SOURCECONDA" ]
 # then
@@ -26,6 +32,7 @@ python msa_pacbio.py -d . -o output/H2B_barcode_variant_map_msa.txt \
 #	exit 1
 # fi
 
+## Install and activate conda environment
 # ENVCHECK=$(conda env list | grep "msa_ccs")
 # source $SOURCECONDA/etc/profile.d/conda.sh
 # if [ -z "$ENVCHECK" ]
@@ -40,9 +47,12 @@ python msa_pacbio.py -d . -o output/H2B_barcode_variant_map_msa.txt \
 # conda activate msa_ccs
 # echo "Environment activated"
 
-# example for running locally
-# python msa_pacbio.py -d ./output -o SUL1_test100_barcode_variant_map.txt \
-# 	--highQual input/SUL1_test100_combined_minQ0_assignment.tsv \
-# 	--inputSeqs input/SUL1_test100_seq_barcodes_filtered.txt \
-#	-c 2 -t 0.6 -v \
+## example for running locally
+## change directory location for the options -m and -n
+## files specified in --highQual and --inputSeqs are for the test case; be sure to change it for your specific library.
+## unzip H2B_seq_barcodes.txt.gz
+# python msa_pacbio.py -d ./output -o H2B_barcode_variant_map_msa.txt \
+# 	--highQual input/H2B_combined_minQ0_assignment.tsv \
+# 	--inputSeqs input/H2B_seq_barcodes.txt \
+#	-c 1 -t 0.6 -v \
 # 	-m ../../muscle/muscle -n ../../emboss/EMBOSS-6.6.0/emboss/needle
