837b5e524db33d3a98a95b8826c39ebdcf981fbf
Chiann-Ling Cindy Yeh <cyeh13@gmail.com>
2021-11-16 15:53:31 -0800

    Update and rename driver_msa.sh to driver_pacrat.sh

--- driver_pacrat.sh
+++ driver_pacrat.sh
@@ -12,7 +12,7 @@
 
 
 ## ******* Section 1: For CentOS7 on the Genome Sciences cluster ******* #
-## Be sure the comment this section out if you are running locally or on a different cluster environment
+## Use this section out if you are running locally or on a different cluster environment
 # module load python/3.7.7
 # module load numpy/1.19.2
 # module load biopython/1.77
@@ -33,7 +33,7 @@
 ## ******* Section 2: Running locally with conda environment ******* #
 ## This is a sample for running PacRAT locally. We have here a skeleton of necessary steps you need to take in order to run PacRAT. Customize as needed.
 ## Please look at README file for directions on installing MUSCLE and NEEDLE. You must specify the location of the software.
-## Uncomment this section if you're choosing to run locally
+## Use this section if you're choosing to run locally
 
 # Validate that Anaconda/Miniconda are installed
 SOURCECONDA=$(conda info --base)
@@ -62,7 +62,7 @@ echo "Environment activated"
 # change directory location for the options -m and -n
 # files specified in --highQual and --inputSeqs are for the test case; be sure to change it for your specific library.
 # unzip H2B_seq_barcodes.txt.gz
-python msa_pacbio.py -d ./output -o H2B_barcode_variant_map_msa.txt \
+python pacrat.py -d ./output -o H2B_barcode_variant_map_msa.txt \
 	--highQual ../input/H2B_highQual_seqs_1000_barcodes.tsv \
 	--inputSeqs ../input/H2B_reads_1000_barcodes.txt \
 	-c 1 -t 0.6 -s
