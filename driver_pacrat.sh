8c68a419f10963a1a94fd2b13a8a0672b3e31dc0
Chiann-Ling Cindy Yeh <cyeh13@gmail.com>
2022-01-04 12:14:39 -0800

    minor typo, cluster option changed
    numpy no longer available as a module on GS grid

--- driver_pacrat.sh
+++ driver_pacrat.sh
@@ -12,9 +12,9 @@
 
 
 ## ******* Section 1: For CentOS7 on the Genome Sciences cluster ******* #
-## Use this section out if you are running locally or on a different cluster environment
+## Comment this section out if you are running locally or on a different cluster environment
 # module load python/3.7.7
-# module load numpy/1.19.2
+# module load numpy/1.19.2 # numpy module no longer on GS cluster, install via pip or conda (see Section 2 below for installing through conda)
 # module load biopython/1.77
 # module load joblib/0.15.1
 # 
